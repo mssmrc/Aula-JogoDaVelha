3cba9a8dcba903e70ac5ee9f37225a618f9a27d7
mssmrc <souza.moisesm@gmail.com>
2018-07-25 11:07:50 -0300

    ajustar detalhes

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
 const casas = document.querySelectorAll(".casa");
+const tabuleiro = document.querySelector("#tabuleiro");
+const sectionsInputs = document.querySelector("#inputs");
+const botaoIniciar = document.querySelector("button"); 
+const inputs = document.querySelector("input");
+
 let contador = 0;
+let jogador1;
+let jogador2;
+let simbolo1;
+let simbolo2;
+
+botaoIniciar.onclick = validarInicio;
+
+//function contador = 0;
+
+function validarInicio(){
+    for(input of inputs){
+        if(input.value === ""){
+            alert("Preencha todos os campos e só depois clique em Iniciar Jogo.");
+            return;
+        }
+    }
+    iniciarJogo()
+}
+
+function iniciarJogo(){
+    jogador1 = inputs[0].value;
+    jogador2 = inputs[2].value;
+    simbolo1 = inputs[1].value;
+    simbolo2 = inputs[3].value;
+
+    for(let casa of casas){
+        casa.onclik = clicar;
+        casa.innerHTML = "";
+    }
+    tabuleiro.style.display = "block";
+    sectionsInputs.style.display = "flex";
+}
 
 for(let casa of casas){
     casa.onclick = clicar;
@@ -14,11 +51,16 @@ function verificar(){
     comparar(casas[2], casas [5], casas[8]) ||
     comparar(casas[0], casas [4], casas[8]) ||
     comparar(casas[2], casas [4], casas[6])){
+        for(let casa of casas){
+            casa.onclick = null;
+        }
         if(contador % 2){
-            document.querySelector("body").style.backgroundColor = "blue";
+            document.querySelector("#vencedor").style.innerHTML = "jogador1";
+            document.querySelector("#winners").style.transform = "translateY(0)";
         }
         else{
-            document.querySelector("body").style.backgroundColor = "green";
+            document.querySelector("#vencedor").style.innerHTML = "jogador2";
+            document.querySelector("#winners").style.transform = "translateY(0)";
         }
         
     }
@@ -41,11 +83,11 @@ function clicar(){
     contador++;
     
     if(contador % 2){
-        this.innerHTML= "X";
+        this.innerHTML= simbolo1;
         this.style.color = "blue";
     }
     else{
-        this.innerHTML= "O";
+        this.innerHTML= simbolo2;
         this.style.color = "green";
     }
 
